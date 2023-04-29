15792dfbcec9835ff2262d0fb0c042cfafa64f09
Furkan Ertekin <furkanertekin0@gmail.com>
2023-04-29 15:47:05 +0300

    entity eklendi, repository eklendi ve dependency injection eklendi.

---
+++ DataAccess/Concrete/EntityFramework/Contexts/TravelersContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class TravelersContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server:(localdb)\mssqllocaldb;Database:Travelers;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}

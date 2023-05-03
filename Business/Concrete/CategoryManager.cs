7d48a897dbb3eae03b5881cb8bf8249dc4914917
Furkan Ertekin <furkanertekin0@gmail.com>
2023-05-03 18:46:09 +0300

    kategori için endpoint yazıldı.

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstract;
+using Business.Contants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        private ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public IDateResult<List<Category>> GetList()
+        {
+            return new SuccessDataResult<List<Category>>(_categoryDal.GetList().ToList());
+        }
+        public IResult Add(Category category)
+        {
+            _categoryDal.Add(category);
+            return new SuccessResult(Messages.CategoryAdded);
+        }
+
+        public IResult Delete(Category category)
+        {
+            _categoryDal.Delete(category);
+            return new SuccessResult(Messages.CategoryDeleted);
+        }
+
+        public IResult Update(Category category)
+        {
+            _categoryDal.Update(category);
+            return new SuccessResult(Messages.CategoryUpdated);
+        }
+    }
+}
--- DataAccess/Concrete/EntityFramework/Contexts/TravelersContext.cs
+++ DataAccess/Concrete/EntityFramework/Contexts/TravelersContext.cs
@@ -16,5 +16,6 @@ namespace DataAccess.Concrete.EntityFramework.Contexts
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }

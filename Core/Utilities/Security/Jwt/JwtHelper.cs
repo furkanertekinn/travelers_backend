7ad9a53ad59f1641bf47db4d3c35569fd4306956
Furkan Ertekin <furkanertekin0@gmail.com>
2023-05-04 23:23:21 +0300

    JVT mimarisi kurulmaya başlandı.

---
+++ Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Security.Encyption;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Jwt
+{
+    public class JwtHelper : ITokenHelper
+    {
+        public IConfiguration Configuration { get; }
+        public TokenOptions _tokenOptions { get; set; }
+
+        public JwtHelper(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _tokenOptions = Configuration.GetSection(key: "TokenOptions").Get<TokenOptions>();
+        }
+
+        public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
+        {
+            var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
+        }
+    }
+}
